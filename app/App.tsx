76e658ee66714df504fde7250f4ae7fdd587f8b6
Artem Sadikov <marolboron@ya.ru>
2021-03-21 23:23:33 +0300

    feat(splash): :fire: added splashes

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,13 @@
+import React, { useEffect } from "react";
+import { View } from "react-native";
+import SplashScreen from "react-native-splash-screen";
+
 const App = () => {
-  return null;
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return <View style={{ flex: 1, backgroundColor: "red" }} />;
 };
 
 export default App;
