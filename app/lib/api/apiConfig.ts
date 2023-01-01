99d25bad5142e7a35a6d3741afc65037ae1b58bb
Aslan Evloev <yevloevaslan@gmail.com>
2023-01-01 23:04:58 +0000

    Update apiConfig.ts, change baseURL from translate.use-case.ru to samott.ru

--- app/lib/api/apiConfig.ts
+++ app/lib/api/apiConfig.ts
@@ -4,7 +4,7 @@ declare const global: any;
 interface ApiConfig extends Config {}
 
 const config: ApiConfig = {
-  baseURL: "https://translate.use-case.ru/api/",
+  baseURL: "https://samott.ru/api/",
   logger: __DEV__ && Boolean(global.origin),
   headers: {
     "content-Type": "application/json",
